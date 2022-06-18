f815a0e3eee72a51645a85ddfde13ce30b9813a1
unknown <3236875710511@ingenieria.usac.edu.gt>
2022-06-18 13:40:56 -0600

    Obtener datos del formulario login

--- src/Js/home.js
+++ src/Js/home.js
@@ -3,19 +3,30 @@ const login=document.querySelector("#singin");
 
 const form=document.getElementById("formulario");
 
-/*este pedazo funciona esconde y aparece todo el body*/
 
+/*-------------------------------------------------------
+Codigo para aparecer y desaparecer el formulario de login
+--------------------------------------------------------*/
 login.addEventListener("click", (e) =>{
     e.preventDefault();
-    console.log("Click en singin")
+    /*console.log("Click en singin")*/
     /*document.body.classList.toggle("show-login");*/
     form.classList.toggle('show-login');
 });
 
+/*----------------------------------
+Codigo para el ingreso de un usuario
+------------------------------------*/
 
-/*
-const open=document.getElementById("singin");
-const formu=document.getElementById("formulario");
-const openForm = () => formu.classList.add("show-login");
-
-open.addEventListener("click",openForm);*/
+form.onsubmit = function(e){
+   
+  e.preventDefault();
+  const user = document.getElementById("username").value;
+  const pass=document.getElementById("pass").value;
+  form.reset();
+  console.log(user);
+  console.log(pass);
+  
+  
+}
+    
